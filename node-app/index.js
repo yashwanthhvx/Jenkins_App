d75c495da35c0e5665661480ae1bd52ec7507cd1
yashwanthhvx <135807671+yashwanthhvx@users.noreply.github.com>
2023-06-08 11:32:11 +0530

    Update index.js

--- node-app/index.js
+++ node-app/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World, This is node-app1');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port 3000`);
 });
